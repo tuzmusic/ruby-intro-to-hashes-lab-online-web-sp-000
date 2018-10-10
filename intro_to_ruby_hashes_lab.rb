c7744fd41e6e8c4aa67daf9da62ac631ef4dd91d
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-10 23:53:20 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,11 +9,20 @@ end
 def monopoly
 	monopoly = {
 		railroads: {
-		},
-
 			pieces: 4,
-			names: {},
-			rent_in_dollars: {}
+			names: {
+				reading_railroad: {},
+				pennsylvania_railroad: {},
+				b_and_o_railroad: {},
+				shortline: {}
+			},
+			rent_in_dollars: {
+				one_piece_owned: 25
+				two_pieces_owned: 50
+				three_pieces_owned: 100
+				four_pieces_owned: 200
+			}
+		}
 	}
 
 end
