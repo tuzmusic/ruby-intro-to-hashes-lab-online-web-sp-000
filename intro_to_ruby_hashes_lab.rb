c1c6ebe93a99454f0ed41bfc6faabbed11043dca
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-10 23:56:40 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,10 +34,10 @@ def monopoly_with_third_tier
 				shortline: {},
 			},
 			rent_in_dollars: {
-				one_piece_owned: 25
-				two_pieces_owned: 50
-				three_pieces_owned: 100
-				four_pieces_owned: 200
+				one_piece_owned=> 25
+				two_pieces_owned=> 50
+				three_pieces_owned=> 100
+				four_pieces_owned=> 200
 			}
 		}
 	}
@@ -54,10 +54,10 @@ def monopoly_with_fourth_tier
 				shortline: {"mortgage_value"=>"$800"},
 			},
 			rent_in_dollars: {
-				one_piece_owned: 25
-				two_pieces_owned: 50
-				three_pieces_owned: 100
-				four_pieces_owned: 200
+				one_piece_owned=> 25
+				two_pieces_owned=> 50
+				three_pieces_owned=> 100
+				four_pieces_owned=> 200
 			}
 		}
 	}
